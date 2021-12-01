a89d0e1a5a93cb02aa2af7c7d5bf80d686c31a4a
Curilang <curilang30@gmail.com>
2021-12-01 00:48:22 -0600

    UPDATE 20211130

--- BGAnimations/ScreenSelectCharacters overlay.lua
+++ BGAnimations/ScreenSelectCharacters overlay.lua
@@ -1,6 +1,124 @@
--- DO NOT USE THIS IN ANOTHER THEME! STRICTLY PROHIBITED!
+local t = Def.ActorFrame{};
 
-local t=Def.ActorFrame{}
+t[#t+1] = LoadActor(THEME:GetPathB("","ModDate"));
+
+--P1
+t[#t+1] = Def.ActorFrame{
+	InitCommand=cmd(x,SCREEN_CENTER_X-320;y,SCREEN_CENTER_Y+14);
+	OffCommand=cmd(sleep,0.2;linear,0.2;zoomy,0);
+		LoadActor( THEME:GetPathG("","ScreenSelectProfile/BG01"))..{
+			InitCommand=cmd(y,-2;shadowlength,0;zoomy,0.85)
+		};
+		LoadActor( THEME:GetPathG("","ScreenSelectProfile/BG02") ) .. {
+			InitCommand=cmd(y,-0)
+		};
+		LoadActor( THEME:GetPathG("","ScreenSelectProfile/"..ddrgame.."sceawi_frame_top") ) .. {
+			InitCommand=function(s) s:valign(1.04):zoomy(1) end,
+			OnCommand=function(self)
+				if ddrgame=="blue_" then
+					self:y(-233);
+				else
+					self:y(-238);
+				end;
+				self:zoomy(0.9);
+			end;
+		};
+		LoadActor(THEME:GetPathG("","ScreenSelectProfile/"..ddrgame.."sceawi_frame_head_1p")) .. {
+			InitCommand=cmd(y,-265);
+		};
+		LoadActor(THEME:GetPathG("","ScreenSelectProfile/"..ddrgame.."sceawi_frame_bottom")) .. {
+			InitCommand=cmd(valign,0;y,224);
+		};
+		LoadActor(THEME:GetPathG("","ScreenSelectProfile/"..ddrgame..lang.."_sceawi_scene_guide_keyoperation")) .. {
+			InitCommand=function(self)
+				self:x(0);
+					if ddrgame=="gold_" then
+						self:y(270)
+					else
+						self:y(276)
+					end;
+				self:zoom(1);
+			end;
+		};
+	};
+
+--P2
+t[#t+1] = Def.ActorFrame{
+	InitCommand=cmd(x,SCREEN_CENTER_X+320;y,SCREEN_CENTER_Y+14);
+	OffCommand=cmd(sleep,0.2;linear,0.2;zoomy,0);
+		LoadActor( THEME:GetPathG("","ScreenSelectProfile/BG01"))..{
+			InitCommand=cmd(y,-2;shadowlength,0;zoomy,0.85)
+		};
+		LoadActor( THEME:GetPathG("","ScreenSelectProfile/BG02") ) .. {
+			InitCommand=cmd(y,-0)
+		};
+		LoadActor( THEME:GetPathG("","ScreenSelectProfile/"..ddrgame.."sceawi_frame_top") ) .. {
+			InitCommand=function(s) s:valign(1.04):zoomy(1) end,
+			OnCommand=function(self)
+				if ddrgame=="blue_" then
+					self:y(-233);
+				else
+					self:y(-238);
+				end;
+				self:zoomy(0.9);
+			end;
+		};
+		LoadActor(THEME:GetPathG("","ScreenSelectProfile/"..ddrgame.."sceawi_frame_head_2p")) .. {
+			InitCommand=cmd(y,-265);
+		};
+		LoadActor(THEME:GetPathG("","ScreenSelectProfile/"..ddrgame.."sceawi_frame_bottom")) .. {
+			InitCommand=cmd(valign,0;y,224);
+		};
+		LoadActor(THEME:GetPathG("","ScreenSelectProfile/"..ddrgame..lang.."_sceawi_scene_guide_keyoperation")) .. {
+			InitCommand=function(self)
+				self:x(0);
+					if ddrgame=="gold_" then
+						self:y(272)
+					else
+						self:y(276)
+					end;
+				self:zoom(1);
+			end;
+		};
+	};
+	
+	
+for _,pn in pairs(GAMESTATE:GetEnabledPlayers()) do
+t[#t+1] = Def.ActorFrame{
+	InitCommand=function(s) s:xy(pn==PLAYER_1 and _screen.cx-320 or _screen.cx+320,_screen.cy+14) end,
+	OffCommand=function(s) s:sleep(0.2):linear(0.2):zoomy(0) end,
+		LoadActor(THEME:GetPathG("","ScreenSelectProfile/Tex_0018")) .. {
+			InitCommand=cmd(y,-160);
+		};
+		LoadActor( THEME:GetPathG("","ScreenSelectProfile/Arrow.png") ) .. {
+			InitCommand=cmd(bounce;effectmagnitude,4,0,0;effectperiod,0.9;x,-140;y,-160;zoom,1;);
+		};
+		LoadActor( THEME:GetPathG("","ScreenSelectProfile/Arrow.png"))..{
+			InitCommand=cmd(bounce;effectmagnitude,-4,0,0;effectperiod,0.9;x,140;y,-160;zoomx,-1);
+		};
+	};
+end;
+
+
+if GAMESTATE:IsPlayerEnabled(PLAYER_1) and not GAMESTATE:IsPlayerEnabled(PLAYER_2) then
+t[#t+1] = Def.ActorFrame{
+		InitCommand=function(s) s:xy(_screen.cx+320,_screen.cy+14) end,
+		LoadActor( THEME:GetPathG("","ScreenSelectProfile/"..ddrgame.."sceawi_press_button") ) .. {
+			InitCommand=cmd(y,-20;diffuseshift;effectcolor1,Color('White');effectcolor2,color("#A5A6A5"));
+			OffCommand=cmd(linear,0.1;zoomy,0;diffusealpha,0);
+		};
+	};
+end;
+
+if GAMESTATE:IsPlayerEnabled(PLAYER_2) and not GAMESTATE:IsPlayerEnabled(PLAYER_1) then
+t[#t+1] = Def.ActorFrame{
+		InitCommand=function(s) s:xy(_screen.cx-320,_screen.cy+14) end,
+		LoadActor( THEME:GetPathG("","ScreenSelectProfile/"..ddrgame.."sceawi_press_button") ) .. {
+			InitCommand=cmd(y,-20;diffuseshift;effectcolor1,Color('White');effectcolor2,color("#A5A6A5"));
+			OffCommand=cmd(linear,0.1;zoomy,0;diffusealpha,0);
+		};
+	};
+end;
 
 -- Character List!
 local CharList = CHARMAN:GetAllCharacters();
@@ -122,26 +240,16 @@ local function TestActorScroller()
 				Bones = value:GetRestAnimationPath();
 				InitCommand=function(self)
 					self:cullmode("CullMode_None"):y(_screen.cy-290):rotationy(180)
-				if string.find( value:GetDisplayName(), "Baby") then
-					self:zoom(5.4)
-				elseif string.find( value:GetDisplayName(), "Rinon") then
-					self:zoom(8.7)
-				elseif string.find( value:GetDisplayName(), "Rena") then
-					self:zoom(12)   
-				elseif string.find( value:GetDisplayName(), "Julio") then
-					self:zoom(11)
-				elseif string.find( value:GetDisplayName(), "PiX") then
-					self:zoom(5)
-				elseif string.find( value:GetDisplayName(), "[HP]") then
-					self:zoom(12)
-				elseif string.match(value:GetDisplayName(), "(X)") or 
-					   string.match(value:GetDisplayName(), "(X2)") or 
-					   string.match(value:GetDisplayName(), "(Ace)") or 
-					   string.match(value:GetDisplayName(), "(2nd)") then
-					self:zoom(13) 
-				else 
-					self:zoom(13) 
-				end;
+				function CharacterInfo(Read,pn)
+						local CharaCfg = value:GetCharacterDir().."CharacterInfo.cfg";
+						local Info = Config.Load(Read,CharaCfg)
+						return Info
+					end
+					if FILEMAN:DoesFileExist(value:GetCharacterDir().."CharacterInfo.cfg") then
+						self:zoom((tonumber(CharacterInfo("Size",value)))*13)
+					else
+						self:zoom(10)
+					end;
 				end,
 			OffCommand=function(s) s:linear(0.2):diffusealpha(0) end,
 			};
