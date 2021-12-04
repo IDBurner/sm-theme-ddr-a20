7b74e2502de36e896ffd4bab507182a217564fc9
Enciso0720 <85234775+Enciso0720@users.noreply.github.com>
2021-12-03 22:28:46 -0500

    FIXED BLACK SCREEN

--- BGAnimations/ScreenGameplay background/default.lua
+++ BGAnimations/ScreenGameplay background/default.lua
@@ -24,6 +24,11 @@ if HasVideo then
 local DanceStagesDir = FILEMAN:GetDirListing("/DanceStages/", true, false)
 local DanceStageSelectedNumber = tonumber(GetUserPref("OptionRowStage"))
 
+	
+if GetUserPref("OptionRowOptionStage")=='OFF' then
+    DanceStageSelectedNumber=1
+end
+	
 
 if DanceStageSelectedNumber == 1 then
     DanceStage = DanceStageSong()
